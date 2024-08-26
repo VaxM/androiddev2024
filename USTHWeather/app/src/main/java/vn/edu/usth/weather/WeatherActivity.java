4d5e47ad9715c1c745cf4b958e4213f64d58d5d7
VaxM <vinhtt.22bi13474@usth.edu.vn>
2024-08-26 15:08:16 +0700

    Practical 2

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -7,18 +7,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_weather);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("start", "On start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("resume", "On resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pause", "On pause");
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stop", "On stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("destroy", "On destroy");
     }
 }
