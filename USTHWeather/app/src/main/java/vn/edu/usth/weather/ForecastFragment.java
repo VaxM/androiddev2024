b2a8af4237db8f703be73382a2d262f8d1c911c3
VaxM <vinhtt.22bi13474@usth.edu.vn>
2024-08-30 19:22:54 +0700

    Redo up to Practical Work 3

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -8,16 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.graphics.Color;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -67,26 +57,13 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Create a new LinearLayout
-        LinearLayout layout = new LinearLayout(getActivity());
-        layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setBackgroundColor(Color.parseColor("#20FF0000")); // Set the background color (optional)
-
-        // Create a new TextView
-        TextView textView = new TextView(getActivity());
-        textView.setText("Thursday");
-        textView.setTextSize(24);
-        textView.setTextColor(Color.BLACK);
-
-        // Create a new ImageView
-        ImageView imageView = new ImageView(getActivity());
-        imageView.setImageResource(R.drawable.weather_news); // Replace with your actual drawable resource name
-
-        // Add the TextView and ImageView to the LinearLayout
-        layout.addView(textView);
-        layout.addView(imageView);
-
-        return layout;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Create a new View
+        View view = new View(getActivity());
+        view.setBackgroundColor(Color.parseColor("#20FF0000"));
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_forecast, container, false);
+        return view;
     }
 }
